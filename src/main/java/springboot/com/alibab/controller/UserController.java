cf6dce4f71588aec77ab506b7602bd3696ac5ded
liuzilong <helloworld_long@163.com>
2019-03-11 21:54:48 +0800

    aaaa

--- src/main/java/springboot/com/alibab/controller/UserController.java
+++ src/main/java/springboot/com/alibab/controller/UserController.java
@@ -2,6 +2,8 @@ package springboot.com.alibab.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +17,14 @@ import springboot.com.alibab.service.UserService;
 @RequestMapping("/user")
 public class UserController {
 	
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	private UserService userService;
 	
 	@RequestMapping("/all")
 	public String getAllUser(){
+		log.info("controller in ");
 		List<User> allUser = userService.getAllUser();
 		return JSON.toJSONString(allUser);
 	}
