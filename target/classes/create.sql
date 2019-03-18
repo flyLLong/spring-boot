07014e32277ad0372017354126b71d2c717e770b
liuzilong <helloworld_long@163.com>
2019-03-18 21:48:06 +0800

    all

--- target/classes/create.sql
+++
@@ -1,9 +0,0 @@
-create table t_user (
-	user_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	user_name VARCHAR(25) NOT NULL UNIQUE,
-	password VARCHAR(25) NOT NULL,
-	phone VARCHAR(25) NOT NULL UNIQUE,
-	create_time datetime DEFAULT NULL
-)ENGINE=INNODB DEFAULT CHARSET=UTF8;
-
-INSERT INTO t_user (user_name, password, phone) VALUES ('zhaoyun', '123456', '13688886666');
